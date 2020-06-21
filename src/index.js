de7455f4e006fdf3174d8d10aff988553934f8fc
Matheus <matheuslino86a@gmail.com>
2020-06-21 12:00:55 -0400

    alterações

--- src/index.js
+++ src/index.js
@@ -54,7 +54,6 @@ io.on('connection', (socket) => {
         io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
-    
 
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
